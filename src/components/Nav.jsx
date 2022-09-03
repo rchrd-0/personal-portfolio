33999e86171b1dd3c66b87e5ebd6bd14cc8b34ed
richard <richard.ytli+f@gmail.com>
2022-09-03 18:23:16 +0800

    Add nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Nav = () => {
-  return <nav id="nav">nav placeholder; sticky</nav>;
+  return (
+    <nav>
+      <div id="left">
+        rchrd<span>.co</span>
+      </div>
+      <div id="right">
+        <ul>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
